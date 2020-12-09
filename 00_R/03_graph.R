533866a3272b55f9df01f81b80e6606814176d3d
Diana Tan <diana.w.tan@gmail.com>
2020-12-09 18:22:12 +0800

    update

--- 00_R/03_graph.R
+++ 00_R/03_graph.R
@@ -6,6 +6,23 @@ data <- read_csv("02_clean_data/02_clean_transf_data.csv") %>%
   filter(PARENT_ID != "2011125MO")
 
 data$group_new <- paste(data$group, data$sex)
+data$normdist <- as.numeric(data$normdist)
 
-ggplot(data) +
-  geom_density(aes(x = masc_score, fill = group_new))
+asfarParents <- ggplot(data, aes(x = masc_score, y = normdist, colour = group_new)) +
+  geom_point(aes(shape = group_new), size = 2) +
+  scale_shape_manual(values = c(3, 1, 2, 8)) +
+  geom_line(size = 0.8) +
+  scale_color_brewer(palette = "Dark2") +
+  theme_classic() +
+  theme(legend.title = element_blank()) +
+  labs(x = "Facial Masculinity Score", y = "Probability Density")
+
+print(asfarParents)
+
+
+ggsave("asfarParentsMasc.png", 
+       plot = asfarParents,
+       units = "cm",
+       dpi = 1000,
+       width = 21,
+       height = 10)

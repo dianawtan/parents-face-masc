62ca0ab63cad9506bc734eea873022e014d9a9d2
Diana Tan <diana.w.tan@gmail.com>
2021-02-03 18:06:17 +0800

    update

--- 00_R/02_mainAnalyses.R
+++ 00_R/02_mainAnalyses.R
@@ -12,7 +12,7 @@
 
 #### libraries ----
 library(pacman)
-p_load(psych, readr, tidyverse, sjstats, pwr)
+p_load(psych, readr, tidyverse, sjstats, pwr, effsize)
 
 
 #### data import ----
@@ -26,18 +26,31 @@ describeBy(x = data, group = c("group", "sex"), digits = 3)
 
 #### main analyses ----
 
-## comparing between fathers and controls
+## comparing between fathers (n=58) and controls (n=50)
 
 dataM <- data %>%
   filter(sex == "male") 
 
+dataM$masc_score <- as.numeric(dataM$masc_score)
+
 t.test(data = dataM, masc_score ~ group)
 cohen.d(dataM$masc_score ~ dataM$group)
-cohen.d.ci()
-## comparing between mothers and controls
+# d = 0.6052191
+cohen.d.ci(d = 0.6052191, n1 = 58, n2 = 50)
+
+
+## comparing between mothers (n=134) and controls (n=113)
 
 dataF <- data %>%
   filter(sex == "female") 
 
 t.test(data = dataF, masc_score ~ group)
+cohen.d(dataF$masc_score ~ dataF$group)
+# d = 0.3754097
+cohen.d.ci(d = 0.3754097, n1 = 134, n2 = 113)
 
+## correlations between masc scores and BAPQ total
+
+corr.test(x = dataM$masc_score, y = dataM$bapq_total, method = "pearson")
+
+corr.test(x = dataF$masc_score, y = dataF$bapq_total, method = "pearson")

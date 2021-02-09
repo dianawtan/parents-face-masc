6a03861d4c286a8931f6b0c46ea9e25833757f88
Diana Tan <diana.w.tan@gmail.com>
2021-02-09 17:20:48 +0800

    update

--- 00_R/02_mainAnalyses.R
+++ 00_R/02_mainAnalyses.R
@@ -6,7 +6,7 @@
 
 #### libraries ----
 library(pacman)
-p_load(psych, readr, tidyverse, sjstats, pwr, effsize)
+p_load(psych, readr, tidyverse, sjstats, pwr, DescTools)
 
 
 #### data import ----
@@ -26,24 +26,31 @@ dataM <- data %>%
 dataF <- data %>%
   filter(sex == "female") 
 
+# combined sexes
+
+corr.test(data$masc_score, data$ageAtScan) #r = 0.26, p < .001, n = 355
+corr.test(data$masc_score, data$facial_area) #r = 0.55, p <.001. n = 355
+
+# males only
+
 corr.test(dataM$masc_score, dataM$ageAtScan) #r = 0.26, p < .001, n = 108
 corr.test(dataM$masc_score, dataM$facial_area) #r = 0.24, p = .01. n = 108
 
+# females only
+
 corr.test(dataF$masc_score, dataF$ageAtScan) #r = 0.20, p < .001, n = 247
-corr.test(dataF$masc_score, dataF$facial_area) #r = 0.38, p = < .001, n = 247
+corr.test(dataF$masc_score, dataF$facial_area) #r = 0.38, p < .001, n = 247
 
 
 #### main analyses ----
 
 ## before controlling for age and facial area
+
 summary(aov(masc_score ~ group*sex, data = data))
-summary(aov(masc_score ~ group, data = dataM))
-summary(aov(masc_score ~ group, data = dataF))
+eta_sq(aov(masc_score ~ group*sex, data = data), partial = TRUE, ci.lvl = 0.95)
 
 ## after controlling for age and facial area
 
 summary(aov(masc_score ~ group*sex + ageAtScan + facial_area, data = data))
-summary(aov(masc_score ~ group + ageAtScan + facial_area, data = dataM))
-summary(aov(masc_score ~ group + ageAtScan + facial_area, data = dataF))
-
+eta_sq(aov(masc_score ~ group*sex + ageAtScan + facial_area, data = data), partial = TRUE, ci.lvl = 0.95)
 

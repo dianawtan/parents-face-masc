d98cd6b736697807c0bd91ac1d7b39e33b101da6
Diana Tan <diana.w.tan@gmail.com>
2021-12-21 18:48:05 +1100

    changes for r1

--- 00_R/01_prelimAnalyses.R
+++ 00_R/01_prelimAnalyses.R
@@ -16,6 +16,58 @@ clean_data <- read_csv("02_clean_data/01_clean_data.csv")
 
 # density plots
 
-ggdensity(clean_data$masc_score) # bimodel because sexual dimorphism
+ggdensity(clean_data$masc_score) # bimodel because sexual dimorphism, no transformation needed
 
-# no transformation needed
+ggdensity(clean_data$facial_area) #normally distributed
+ggdensity(clean_data$g_n_sto) #normally distributed
+ggdensity(clean_data$g_ex_ex) #normally distributed
+ggdensity(clean_data$g_ft_ft) #right skew
+ggdensity(clean_data$g_sto_pg) #right skew
+ggdensity(clean_data$g_ex_ch_L) #normally distributed
+ggdensity(clean_data$g_ex_ch_R) #normally distributed
+ggdensity(clean_data$l_sbal_sn_sbal) #normally distributed
+ggdensity(clean_data$l_sn_prn) #normally distributed
+ggdensity(clean_data$l_sn_sto) #normally distributed
+ggdensity(clean_data$l_n_prn) #normally distributed
+
+
+#### data transformation ----
+
+# Since distributions are skewed, variables are transformed using Box-Cox transformation
+# create a function for transforming variables using Box Cox procedures
+# https://rcompanion.org/handbook/I_12.html
+
+boxCoxTrans <- function(measure) {
+  model <- boxcox(measure ~ 1,
+                  lambda = seq(-6, 6, 0.1)) # try values -6 to 6 by 0.1 to identify appropriate exponent
+  modelDf <- data.frame(model$x, model$y) # Create a data frame with the results
+  modelDfOrdered <- modelDf[with(modelDf, order(-modelDf$model.y)), ] # Order the new data frame by decreasing y
+  modelDfOrdered[1, ] # Display the lambda with the greatest log likelihood
+  lambda <- modelDfOrdered[1, "model.x"]
+  
+  if(lambda == 0) log10(measure)
+  else (measure ^ lambda - 1) / lambda
+  
+}
+
+# create a dataframe of measures to be transformed
+
+faceVar <- clean_data %>%
+  select(g_ft_ft, g_sto_pg)
+
+transFaceVar <- map(.x = faceVar, .f = boxCoxTrans) %>%
+  as.data.frame() %>%
+  rename("g_ft_ft_box" = "g_ft_ft",
+         "g_sto_pg_box" = "g_sto_pg")
+
+# check distribution of transformed variables 
+
+ggdensity(transFaceVar$g_ft_ft_box) # normally distributed
+ggdensity(transFaceVar$g_sto_pg_box) # normally distributed 
+
+clean_data <- bind_cols(clean_data, transFaceVar)
+
+
+#### data export ----
+
+write_csv(clean_data, "02_clean_data/02_clean_transf_data.csv")
--- 00_R/02_mainAnalyses.R
+++ 00_R/02_mainAnalyses.R
@@ -1,6 +1,6 @@
 ## data analysis script
 ## written by Diana Tan 26/08/20
-## last updated on: 04/02/21
+## last updated on: 21/12/21
 
 ## These analyses compared parents' faces versus controls' faces. 
 
@@ -11,20 +11,20 @@ pacman::p_load(psych, readr, tidyverse, sjstats, pwr, DescTools)
 
 #### data import ----
 
-data <- read_csv("02_clean_data/01_clean_data.csv")
+data <- read_csv("02_clean_data/02_clean_transf_data.csv")
 
 
 #### descriptive statistics ----
-describeBy(x = data, group = c("group", "sex"), digits = 3)
+describeBy(x = data, group = c("group", "sex_parent"), digits = 3)
 
 
 #### check for confounding variables ----
 
 dataM <- data %>%
-  filter(sex == "male") 
+  filter(sex_parent == "male") 
 
 dataF <- data %>%
-  filter(sex == "female") 
+  filter(sex_parent == "female") 
 
 # combined sexes
 
@@ -44,14 +44,93 @@ corr.test(dataF$masc_score, dataF$facial_area) #r = 0.38, p < .001, n = 247
 
 #### main analyses ----
 
+### masculinity score
+
 ## before controlling for age and facial area
 
-summary(aov(masc_score ~ group*sex, data = data))
-eta_sq(aov(masc_score ~ group*sex, data = data), partial = TRUE, ci.lvl = 0.95)
+summary(aov(masc_score ~ group*sex_parent, data = data))
+eta_sq(aov(masc_score ~ group*sex_parent, data = data), partial = TRUE, ci.lvl = 0.95)
 
 ## after controlling for age and facial area
 
-summary(aov(masc_score ~ group*sex + ageAtScan + facial_area, data = data))
-eta_sq(aov(masc_score ~ group*sex + ageAtScan + facial_area, data = data), partial = TRUE, ci.lvl = 0.95)
+summary(aov(masc_score ~ group*sex_parent + ageAtScan + facial_area, data = data))
+eta_sq(aov(masc_score ~ group*sex_parent + ageAtScan + facial_area, data = data), partial = TRUE, ci.lvl = 0.95)
 
+
+### facial distances
+
+## before controlling for age and facial area
+
+summary(aov(g_n_sto ~ group*sex_parent, data = data))
+eta_sq(aov(g_n_sto ~ group*sex_parent, data = data), partial = TRUE, ci.lvl = 0.95)
+
+summary(aov(g_ex_ex ~ group*sex_parent, data = data))
+eta_sq(aov(g_ex_ex ~ group*sex_parent, data = data), partial = TRUE, ci.lvl = 0.95)
+
+summary(aov(g_ft_ft_box ~ group*sex_parent, data = data))
+eta_sq(aov(g_ft_ft_box ~ group*sex_parent, data = data), partial = TRUE, ci.lvl = 0.95)
+
+summary(aov(g_sto_pg_box ~ group*sex_parent, data = data))
+eta_sq(aov(g_sto_pg_box ~ group*sex_parent, data = data), partial = TRUE, ci.lvl = 0.95)
+
+summary(aov(g_ex_ch_L ~ group*sex_parent, data = data))
+eta_sq(aov(g_ex_ch_L ~ group*sex_parent, data = data), partial = TRUE, ci.lvl = 0.95)
+
+summary(aov(g_ex_ch_R ~ group*sex_parent, data = data))
+eta_sq(aov(g_ex_ch_R ~ group*sex_parent, data = data), partial = TRUE, ci.lvl = 0.95)
+
+summary(aov(l_sbal_sn_sbal ~ group*sex_parent, data = data))
+eta_sq(aov(l_sbal_sn_sbal ~ group*sex_parent, data = data), partial = TRUE, ci.lvl = 0.95)
+
+summary(aov(l_sn_prn ~ group*sex_parent, data = data))
+eta_sq(aov(l_sn_prn ~ group*sex_parent, data = data), partial = TRUE, ci.lvl = 0.95)
+
+summary(aov(l_sn_sto ~ group*sex_parent, data = data))
+eta_sq(aov(l_sn_sto ~ group*sex_parent, data = data), partial = TRUE, ci.lvl = 0.95)
+
+summary(aov(l_n_prn ~ group*sex_parent, data = data))
+eta_sq(aov(l_n_prn ~ group*sex_parent, data = data), partial = TRUE, ci.lvl = 0.95)
+
+
+## after controlling for age and facial area
+
+summary(aov(g_n_sto ~ group*sex_parent + ageAtScan + facial_area, data = data))
+eta_sq(aov(g_n_sto ~ group*sex_parent + ageAtScan + facial_area, data = data), partial = TRUE, ci.lvl = 0.95)
+
+summary(aov(g_ex_ex ~ group*sex_parent + ageAtScan + facial_area, data = data))
+eta_sq(aov(g_ex_ex ~ group*sex_parent + ageAtScan + facial_area, data = data), partial = TRUE, ci.lvl = 0.95)
+
+summary(aov(g_ft_ft_box ~ group*sex_parent + ageAtScan + facial_area, data = data))
+eta_sq(aov(g_ft_ft_box ~ group*sex_parent + ageAtScan + facial_area, data = data), partial = TRUE, ci.lvl = 0.95)
+
+summary(aov(g_sto_pg_box ~ group*sex_parent + ageAtScan + facial_area, data = data))
+eta_sq(aov(g_sto_pg_box ~ group*sex_parent + ageAtScan + facial_area, data = data), partial = TRUE, ci.lvl = 0.95)
+
+summary(aov(g_ex_ch_L ~ group*sex_parent + ageAtScan + facial_area, data = data))
+eta_sq(aov(g_ex_ch_L ~ group*sex_parent + ageAtScan + facial_area, data = data), partial = TRUE, ci.lvl = 0.95)
+
+summary(aov(g_ex_ch_R ~ group*sex_parent + ageAtScan + facial_area, data = data))
+eta_sq(aov(g_ex_ch_R ~ group*sex_parent + ageAtScan + facial_area, data = data), partial = TRUE, ci.lvl = 0.95)
+
+summary(aov(l_sbal_sn_sbal ~ group*sex_parent + ageAtScan + facial_area, data = data))
+eta_sq(aov(l_sbal_sn_sbal ~ group*sex_parent + ageAtScan + facial_area, data = data), partial = TRUE, ci.lvl = 0.95)
+
+summary(aov(l_sn_prn ~ group*sex_parent + ageAtScan + facial_area, data = data))
+eta_sq(aov(l_sn_prn ~ group*sex_parent + ageAtScan + facial_area, data = data), partial = TRUE, ci.lvl = 0.95)
+
+summary(aov(l_sn_sto ~ group*sex_parent + ageAtScan + facial_area, data = data))
+eta_sq(aov(l_sn_sto ~ group*sex_parent + ageAtScan + facial_area, data = data), partial = TRUE, ci.lvl = 0.95)
+
+summary(aov(l_n_prn ~ group*sex_parent + ageAtScan + facial_area, data = data))
+eta_sq(aov(l_n_prn ~ group*sex_parent + ageAtScan + facial_area, data = data), partial = TRUE, ci.lvl = 0.95)
+
+
+#### additional analyses re: sex of proband children ----
+
+# do parents of autistic boys show more masculine faces than those of autistic girls?
+summary(aov(masc_score ~ sex_child, data = dataF)) # no
+summary(aov(masc_score ~ sex_child, data = dataM)) # no
+
+summary(aov(masc_score ~ sex_child + ageAtScan + facial_area, data = dataF)) # no even after controlling
+summary(aov(masc_score ~ sex_child + ageAtScan + facial_area, data = dataM)) # no even after controlling
 
